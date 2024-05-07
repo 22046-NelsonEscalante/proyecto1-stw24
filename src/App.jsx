a9641ed3b7ccf786a408d3cf6f8725698b0a6491
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2024-05-07 11:27:40 -0600

    Fixed css, can now login to admin. (TODO: create admin panel)

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,143 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const backendURL = 'http://127.0.0.1:3300/blogs/'
 
+const user = 'nel'
+const pass = 'hola'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [blogs, setBlogs] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [postID, setPostID] = useState(1)
+  const [user_in, setUser_in] = useState('')
+  const [pass_in, setPass_in] = useState('')
+
+  console.log(postID)
+
+  async function getBlogs() {
+      let lista_blogs = await fetch(backendURL + postID, {
+          method: 'GET',
+          cache: 'default',
+          mode: 'cors'
+      })
+
+      let blogs_json = await lista_blogs.json()
+
+      setBlogs(blogs_json)
+      setLoading(false)
+  }
+
+  const increase = () => {
+      setPostID(postID + 1)
+  }
+
+  const decrease = () => {
+      if (postID > 1) {
+          setPostID(postID - 1)
+      }
+  }
+
+  const submitLogin = () => {
+    set
+  }
+
+  useEffect(() => {
+      getBlogs()
+  }, [postID])
+
+  console.log(blogs)
+
+  if(loading) {
+      return (
+          <div classNameName="bg">
+          <header className="blogHeader">
+              <h1 className="blogTitle">La música de Spinneta</h1>    
+              <a href="/admin">login</a>
+          </header>
+
+          <div className="blogCard">
+              <img src="./assets/loading.gif" alt="loading" />
+              <button className="nextButton" onClick={increase}>Siguiente &gt;</button>
+              <h1 className="idIndicator">{postID}</h1>
+              <button className="prevButton" onClick={decrease}>&lt; Anterior </button>
+          </div>
+      </div>
+      )
+  }
+
+  if(window.location.pathname === '/admin') {
+
+    if(user_in === user && pass_in === pass) {
+        return (
+            <div className="bg">
+                <header className="blogHeader">
+                    <h1 className="blogTitle">La música de Spinneta</h1>    
+                    <a href="/admin">logout</a>
+                </header>
+      
+                <div className="blogCard">
+                    {blogs.map((blog, index) => {
+                        return (
+                            <div>
+                                <h2>{blog.title}</h2>
+                                <p>{blog.content}</p>
+                            </div>
+                        )
+                    })}    
+                    <button className="nextButton" onClick={increase}>Siguiente &gt;</button>
+                    <h1 className="idIndicator">{postID}</h1>
+                    <button className="prevButton" onClick={decrease}>&lt; Anterior </button>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="bg">
+            <header className="blogHeader">
+                <h1 className="blogTitle">Panel de Admin</h1>    
+                <a href="/">logout</a>
+            </header>
+  
+            <div className="blogCard">
+
+                <form onSubmit={submitLogin}>
+                    <div className="field1">
+                        <h3>Usuario: <input id="user_in" value={user_in} onChange={(e) => setUser_in(e.target.value)}></input></h3>
+                    </div>
+                    <div className="field2">
+                        <h3>Contraseña: <input id="pass_in" type="password" onChange={(e) => setPass_in(e.target.value)}></input></h3>
+                    </div>
+                    <button className="submitButton" type='submit'>Submit</button>
+                </form>
+                
+            </div>
+        </div>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg">
+          <header className="blogHeader">
+              <h1 className="blogTitle">La música de Spinneta</h1>    
+              <a href="/admin">login</a>
+          </header>
+
+          <div className="blogCard">
+              {blogs.map((blog, index) => {
+                  return (
+                      <div>
+                          <h2>{blog.title}</h2>
+                          <p>{blog.content}</p>
+                      </div>
+                  )
+              })}    
+              <button className="nextButton" onClick={increase}>Siguiente &gt;</button>
+              <h1 className="idIndicator">{postID}</h1>
+              <button className="prevButton" onClick={decrease}>&lt; Anterior </button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
